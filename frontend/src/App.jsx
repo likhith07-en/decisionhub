eb19b2aa8811e27e7f9b662bd044bef6336e8356
P Likhith Kumar <pujarilikhith07@gmail.com>
2026-07-30 23:48:45 +0530

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,13 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
-
-function Home() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
-      <h1>DecisionHub</h1>
-      <p>Sample frontend for the decision-making platform.</p>
-      <Link to="/dashboard">Go to   hi idn dashboard</Link>
-    </div>
-  );
-}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </BrowserRouter>
